a9c2ecf4d98fe0a0e643c580a739e7e9fb222c9c
MarrySad <mvrsad@gmail.com>
2019-02-18 15:36:12 +0700

    дз к первой лекции

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -4,7 +4,7 @@ const goods = [
     { title: 'Jacket', price: 350 },
     { title: 'Shoes', price: 250 },
   ];
-  const renderGoodsItem = (title, price) => {
+  const renderGoodsItem = (title = 'noname', price = 'не указана') => {
     return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
   }
   const renderGoodsList = (list) => {
