0eb1bf8d242232fdd5961f6ec3aa3f44d5200479
MarrySad <mvrsad@gmail.com>
2019-02-18 16:01:03 +0700

    начало работы над дз ко второму уроку

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,14 +1,36 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-  ];
-  const renderGoodsItem = (title = 'noname', price = 'не указана') => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+class GoodsItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
   }
-  const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+  render() {
+    return `<div
+class="goods-item"><h3> ${ this.title} </h3><p> ${this.price} </p></div>`;
   }
-  renderGoodsList(goods);
+}
+
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+  fetchGoods() {
+    this.goods = [
+      { title: 'Shirt', price: 150 },
+      { title: 'Socks', price: 50 },
+      { title: 'Jacket', price: 350 },
+      { title: 'Shoes', price: 250 },
+    ];
+  }
+  render() {
+    let listHtml = '';
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
