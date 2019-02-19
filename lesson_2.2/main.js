1ec1352ded5ff0fbdc26d7924cd43743573fb32b
MarrySad <mvrsad@gmail.com>
2019-02-19 15:40:08 +0700

    начало работы над 3 заданием

---
+++ lesson_2.2/main.js
@@ -0,0 +1,15 @@
+class Hambuger {
+    constructor(size, stuffing) {
+        
+    }
+}
+
+let hamburgerElem = [
+    {name: 'small', price: 50, calories: 20},
+    {name: 'big', price: 100, calories: 40},
+    {name: 'cheese', price: 10, calories: 20},
+    {name: 'big', price: 100, calories: 40},
+    {name: 'big', price: 100, calories: 40},
+    {name: 'big', price: 100, calories: 40},
+    {name: 'big', price: 100, calories: 40},
+]
