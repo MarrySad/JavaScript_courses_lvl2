9f132c952eb3134bb74368e8c6b680f70277109b
MarrySad <mvrsad@gmail.com>
2019-02-28 22:20:34 +0700

    добавил методы завершения и удаления товара

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -11,81 +11,112 @@ class GoodsItem {
 }
 
 class GoodsList {
-  constructor() {
+  constructor(source = '') {
     this.goods = [];
+    this.source = source;
+    this.listHtml = '';
+  }
+
+  applyData(jsonData) {
   }
 
   fetchGoods() {
-    return new Promise((resolte, reject) => {
-      makeGETRequest(`${API_URL}/catalogData.json`).then((goods) => {
-        this.goods = JSON.parse(goods);
-        resolte();
+    return makeGETRequest(`${API_URL}/${this.source}`)
+      .then((response) => {
+        this.applyData(JSON.parse(response));
       })
-        .catch(() => console.log('Error'))
-    })
+      .catch((response) => {
+      })
   }
 
   render() {
-    let listHtml = '';
-    this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.product_name, good.price);
-      listHtml += goodItem.render();
-    });
-    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+}
+
+class Catalog extends GoodsList {
+  constructor() {
+    super('catalogData.json');
   }
 
   calculatePrice() {
     return this.goods.reduce((a, b) => a + b.price, 0);
   }
+
+  applyData(jsonData) {
+    this.goods = jsonData;
+  }
+
+  render() {
+    this.listHtml = '';
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.product_name, good.price);
+      this.listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = this.listHtml;
+  }
 }
 
-class Cartlist extends GoodsList {
+class Basket extends GoodsList {
   constructor() {
-    super();
-    this.totalPrise = 0;
+    super('getBasket.json');
+    this.totalPrice = 0;
     this.countGoods = 0;
   }
-  addItem(id_product) {
-    var str_json;
-    list.goods.forEach((good) => {
-      if (good.id_product = id_product) {
+
+  applyData(jsonData) {
+    this.goods = jsonData.contents;
+    this.totalPrice = jsonData.amount;
+    this.countGoods = jsonData.countGoods;
+  }
+
+  addGood(id_product) {
+    let str_json;
+    let item;
+    catalog.goods.forEach((good) => {
+      if (good.id_product == id_product) {
+        str_json = JSON.stringify(good);
+        item = good;
+      }
+    })
+    makeGETRequest(`${API_URL}/addToBasket.json`, str_json).then((response) => {
+      if (JSON.parse(response).result === 1) {
+        this.goods.push(item);
+        this.render()
+      }
+    });
+  }
+
+  removeGood(id_product) {
+    let str_json;
+    basket.goods.forEach((good) => {
+      if (good.id_product == id_product) {
         str_json = JSON.stringify(good);
       }
     })
-    makeGETRequest(`${API_URL}/addToBasket.json`, str_json);
-  }
-  removeItem(id_product) {
-  }
-
-  fetchCartGoods() {
-    return new Promise((resolte, reject) => {
-      makeGETRequest(`${API_URL}/getBasket.json`).then((goods) => {
-        console.log(goods);
-        const data = JSON.parse(goods);
-        this.goods = data.contents;
-        this.totalPrise = data.amount;
-        this.countGoods = data.countGoods;
-        resolte();
-      })
-        .catch(() => console.log('Error'))
-    })
+    makeGETRequest(`${API_URL}/addToBasket.json`, str_json).then((response) => {
+      if (JSON.parse(response).result === 1) {
+        const indexToDelete = this.goods.findIndex((item) => item.id_product === id_product);
+        this.render()
+      }
+    });
   }
 
-  calculateSum() {
-  }
   render() {
-    let listHtml = '';
+    this.listHtml = '';
     this.goods.forEach(good => {
       const goodItem = new GoodsItem(good.product_name, good.price);
-      listHtml += goodItem.render();
+      this.listHtml += goodItem.render();
     });
-    document.querySelector('.cart-list').innerHTML = listHtml;
+    this.listHtml += `<div class='cart-price'>Сумма корзины: ${this.totalPrice}</div>`;
+    this.listHtml += `<div class='cart-count'>Кол-во товаров: ${this.countGoods}</div>`;
+    document.querySelector('.cart-list').innerHTML = this.listHtml;
   }
 }
 
 class CartItem extends GoodsItem {
-  constructor(title, price) {
+  constructor(id_product, title, price) {
     super(title, price);
+    this.id_product = id_product;
     this.count = 1;
   }
 }
@@ -98,7 +129,6 @@ function makeGETRequest(url) {
     } else if (window.ActiveXObject) {
       xhr = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    console.log(xhr.readyState)
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4) {
         if (xhr.status !== 200) {
@@ -112,12 +142,13 @@ function makeGETRequest(url) {
   })
 }
 
-const list = new GoodsList();
-list.fetchGoods().then(() => list.render());
+const catalog = new Catalog();
+catalog.fetchGoods().then(() => catalog.render());
 
-let cartlist = new Cartlist();
-cartlist.fetchCartGoods().then(() => cartlist.render());
-cartlist.addItem(123);
-
-
-
+const basket = new Basket();
+basket.fetchGoods().then(() => {
+  basket.render();
+  basket.addGood(123);
+  basket.addGood(456);
+  basket.removeGood(123);
+});
