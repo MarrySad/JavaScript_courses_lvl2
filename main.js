dcf0564c170a1fe84d7ea95f8576519379bf1d3e
MarrySad <mvrsad@gmail.com>
2019-02-28 09:28:08 +0700

    keke

--- main.js
+++
@@ -1,7 +0,0 @@
-//Задание 1 и 2
-
-let str = 'Every royal house or \'dynasty\' has a surname. In Britain\'s case that name is Windsor Queen Elizabeth II is the fourth Windsor monarch. After Queen Elizabeth II, Britain\'s next monarch will probably be Prince Charles, the Prince of Wales. The National Anthem is called \'God save the Queen\'. But if Prince Charles becomes King, it will be \'God save the King\'.\'God save the King\'.';
-const reg = new RegExp('\W\'(.+?)\'\W/', 'g');
-console.log(str);
-str.replace(reg, '"$1"');
-console.log(str);
