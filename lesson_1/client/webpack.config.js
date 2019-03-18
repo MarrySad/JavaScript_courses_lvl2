9f5869686ee31cf2aac5a770d1c0ae0d7c05fe49
MarrySad <mvrsad@gmail.com>
2019-03-18 16:17:51 +0700

    выполнил второе задание

--- lesson_1/client/webpack.config.js
+++ lesson_1/client/webpack.config.js
@@ -1,6 +1,6 @@
-module .exports = {
-    entry : './script/main.js' ,
-    output : {
-    filename : './build.js'
+module.exports = {
+    entry: './script/main.js',
+    output: {
+        filename: './build.js'
     }
-    }
+}
